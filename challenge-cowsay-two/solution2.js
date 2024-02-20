d03690b3066ba96e612151eb1435a8d19c41a4ee
Fathi Kahin <fhkahin@gmail.com>
2024-02-20 11:58:20 +0000

    completed answer 1 and 2

--- challenge-cowsay-two/solution2.js
+++ challenge-cowsay-two/solution2.js
@@ -1,18 +1,34 @@
-// =================
-// Stripped down cowsayer CLI, 
-// no libraries or arguments
-// https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
-// =================
 
-// 1. Make  a command line interface.
+const readline = require("readline");
 
-// 2. Make supplies for our speech bubble
+let rl = readline.createInterface(process.stdin, process.stdout);
 
-// 3. Make a cow that takes a string
+let topLine = "_";
+let bottomLine = "-";
+let saying = "";
 
-const cow = (saying) => {
-    // how did you make the cow before?
-}
+const cow = (inPhrase) => {
+  if (!inPhrase) {
+    inPhrase = "nothing to say?";
+  }
+  saying = "      (" + inPhrase + ")";
+  topLine = "       " + topLine.repeat(inPhrase.length);
+  bottomLine = "       " + bottomLine.repeat(inPhrase.length);
 
-// 4. Use readline to get a string from the terminal 
-// (with a prompt so it's clearer what we want)
+  console.log(topLine);
+  console.log(saying);
+  console.log(bottomLine);
+  console.log("       /");
+  console.log("      /");
+  console.log("^__^ /");
+  console.log("(oo)'_______");
+  console.log("(__)        )-~");
+  console.log("   ||----w |");
+  console.log("   ||     ||");
+};
+
+rl.question("Please enter your saying...", (saying) => {
+  console.log("Your saying is: " + saying);
+  cow(saying);
+  rl.close();
+});
